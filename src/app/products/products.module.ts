18dc7256f23293c0ad0266d9fa83e792f6281232
DEV-COM <kwp@tlen.pl>
2024-12-10 13:51:23 +0100

    Add OrdersModule to manage order-related components; update ProductsModule imports

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductsService } from './products.service';
+import { OrdersModule } from '../orders/orders.module';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    OrdersModule
   ],
   providers: [ProductsService]
 })
