f509c606f47890d396e28d5675803a403d1eb464
DEV-COM <kwp@tlen.pl>
2025-01-17 16:41:13 +0100

    Remove unused test files and add source-map-explorer.
    Test files for `authGuard` and `CopyrightDirective` were deleted as they are no longer necessary. Added `source-map-explorer` to package.json for enhanced bundle analysis, along with its dependencies in package-lock.json. Minor formatting change in `currency-pln.pipe.spec.ts`.

--- src/app/currency-pln.pipe.spec.ts
+++ src/app/currency-pln.pipe.spec.ts
@@ -3,6 +3,6 @@ import { CurrencyPlnPipe } from './currency-pln.pipe';
 describe('CurrencyPlnPipe', () => {
   it('create an instance', () => {
     const pipe = new CurrencyPlnPipe();
-    expect(pipe).toBeTruthy();
+    expect(pipe).toBeTruthy()
   });
 });
