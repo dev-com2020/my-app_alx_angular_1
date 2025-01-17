bfa82326810b23d5cc8fec1d178c32a60df97221
DEV-COM <kwp@tlen.pl>
2025-01-17 14:31:21 +0100

    Refactor product creation form to use reactive forms
    Replaced template-driven form with reactive form in ProductCreateComponent for better scalability and data handling. Updated form controls and simplified the createProduct method to use form group values. Adjusted ProductDetailComponent to fix the binding issue with the price input field.

--- src/app/product-create/product-create.component.ts
+++ src/app/product-create/product-create.component.ts
@@ -1,22 +1,27 @@
 import { Component } from '@angular/core';
 import {ProductsService} from "../products/products.service";
 import {Router} from "@angular/router";
+import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-product-create',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './product-create.component.html',
   styleUrl: './product-create.component.css'
 })
 export class ProductCreateComponent {
+
+  productForm = new FormGroup({
+    title: new FormControl('', {nonNullable:true}),
+    price: new FormControl<number | undefined>(undefined, {nonNullable:true}),
+    categories: new FormControl('', {nonNullable:true})
+  })
+
+
   constructor(private productService: ProductsService, private router: Router) {}
 
-  createProduct(title: string, price: string, categories: string) {
-    this.productService.addProduct({
-      title,
-      price: Number(price),
-      categories
-    }).subscribe(() => this.router.navigate(['/products']));
+  createProduct() {
+    this.productService.addProduct(this.productForm.value).subscribe(() => this.router.navigate(['/products']));
   }
 
 }
