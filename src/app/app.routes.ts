7862e0592e2e77321d1fef1af9e09feea46dd887
DEV-COM <kwp@tlen.pl>
2025-01-17 10:37:14 +0100

    Add product creation feature and improve styles
    Introduce route and service methods for adding new products. Enhance UI with new styles, including header, menu links, and typography updates. Simplified global link styling and added spacer utility class for layout flexibility.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import {Routes} from '@angular/router';
 import {ProductListComponent} from "./product-list/product-list.component";
 import {CartComponent} from "./cart/cart.component";
+import {ProductCreateComponent} from "./product-create/product-create.component";
 
 export const routes: Routes = [
   {path: 'products', component: ProductListComponent},
   {path: 'cart', component: CartComponent},
+  {path: 'products/new', component: ProductCreateComponent},
   // {path: '**', redirectTo: 'products'}
 ];
--- src/app/products/products.service.ts
+++ src/app/products/products.service.ts
@@ -1,6 +1,6 @@
 import { inject, Injectable } from '@angular/core';
 import { Product } from '../product';
-import { Observable, of } from 'rxjs';
+import { Observable, of, map } from 'rxjs';
 import { APP_SETTINGS } from '../app.settings';
 import { HttpClient } from '@angular/common/http';
 
@@ -19,5 +19,19 @@ export class ProductsService {
   getProducts(): Observable<Product[]> {
     return this.http.get<Product[]>(this.productsUrl);
 
+  }
+
+  getProduct(id: number): Observable<Product> {
+    const product = this.products.find(product => product.id === id);
+    return of(product!);
+  }
+
+  addProduct(newProduct: Partial<Product>): Observable<Product> {
+    return this.http.post<Product>(this.productsUrl, newProduct).pipe(
+      map(product => {
+        this.products.push(product);
+        return product;
+      })
+    )
   }
 }
