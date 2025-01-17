2a794fff924d21dac7e3e8f55e7a5987be1d11fd
DEV-COM <kwp@tlen.pl>
2025-01-17 14:11:52 +0100

    Add price update functionality to product detail
    Implement a form in the product detail view to update product prices. Extend the service with an `updateProduct` method to handle price changes via API. Reflect the updated price in the app after submission.

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -9,11 +9,12 @@ import { CurrencyPlnPipe } from '../currency-pln.pipe';
 import {ProductsService} from "../products/products.service";
 import {ActivatedRoute, Router} from "@angular/router";
 import {Observable, switchMap} from "rxjs";
+import {FormsModule} from "@angular/forms";
 
 
 @Component({
     selector: 'app-product-detail',
-    imports: [CommonModule, CurrencyPlnPipe],
+    imports: [CommonModule, CurrencyPlnPipe, FormsModule],
     templateUrl: './product-detail.component.html',
     styleUrl: './product-detail.component.css',
     standalone: true,
@@ -22,6 +23,7 @@ import {Observable, switchMap} from "rxjs";
 export class ProductDetailComponent implements OnInit{
   id = input<number>();
   product$: Observable<Product> | undefined;
+  price: number | undefined;
 
   constructor(
     private productService: ProductsService,
@@ -35,5 +37,13 @@ export class ProductDetailComponent implements OnInit{
         return this.productService.getProduct(Number(params.get('id')));
       })
     )
+  }
+
+  changePrice(product: Product) {
+    this.productService.updateProduct(
+      product.id,
+      this.price!
+    ).subscribe(() => this.router.navigate(['/products']));
+
   }
 }
